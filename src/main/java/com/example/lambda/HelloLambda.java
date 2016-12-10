b689ba571ad838078aa67b1929e1a6020286fd07
Kasim Doctor <kasim.doctor@appdirect.com>
2016-12-10 15:48:49 -0500

    Add code to inspect the Context object and print details

--- src/main/java/com/example/lambda/HelloLambda.java
+++ src/main/java/com/example/lambda/HelloLambda.java
@@ -20,6 +20,16 @@ public class HelloLambda implements RequestHandler<Request, Response> {
         // This log line tests if the globalInt and localInt values remain same/change for subsequent invocations of the function on AWS.
         logger.log(String.format("Global Int++ = %d AND Local Int++ = %d ", ++globalInt, ++localInt));
         
+        inspectContextObject(context);
         return new Response("Hello, " + request.getFirstName() + " " + request.getLastName());
+    }
+    
+    
+    public void inspectContextObject(Context context) {
+        System.out.println("Function name: " + context.getFunctionName());
+        System.out.println("Max mem allocated: " + context.getMemoryLimitInMB());
+        System.out.println("Time remaining in milliseconds: " + context.getRemainingTimeInMillis());
+        System.out.println("CloudWatch log stream name: " + context.getLogStreamName());
+        System.out.println("CloudWatch log group name: " + context.getLogGroupName());
     }
 }
