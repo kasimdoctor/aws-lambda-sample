04dded31722b187e0c7cc4f243b4aaba204ac802
Kasim Doctor <kasim.doctor@appdirect.com>
2016-12-15 14:59:34 -0500

    Add support for S3 events in the function

--- src/main/java/com/example/lambda/HelloLambda.java
+++ src/main/java/com/example/lambda/HelloLambda.java
@@ -1,18 +1,20 @@
 package com.example.lambda;
 
-import java.util.UUID;
+import java.util.List;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.event.S3EventNotification;
 import io.spoonshift.domain.dao.UserDAO;
 import io.spoonshift.domain.model.User;
 import io.spoonshift.utils.db.DynamoDbUtils;
 
 
-public class HelloLambda implements RequestHandler<Request, Response> {
+public class HelloLambda implements RequestHandler<S3Event, Response> {
 
 	private DynamoDB dynamoDB;
 	private UserDAO userDAO;
@@ -24,26 +26,51 @@ public class HelloLambda implements RequestHandler<Request, Response> {
 	}
 
 	@Override
-	public Response handleRequest(Request request, Context context) {
+	public Response handleRequest(S3Event s3event, Context context) {
 		LambdaLogger logger = context.getLogger();
-		logger.log(String.format("Received request %s with UUID: %s ", request.toString(), UUID.randomUUID()));
 
-		System.out.println("Inserting into Table Users");
-		User user = new User();
-		user.setFirstName("Kasim");
-		user.setLastName("Doctor");
-		user.setActive(true);
-		user.setId(1);
-		
-		//userDAO.addUser(User.builder().id(2).firstName("Tom").lastName("Cruise").active(true).build());
-		//userDAO.addUser(User.builder().id(3).firstName("Shahrukh").lastName("Khan").active(true).build());
+		processS3Event(s3event, logger);
+		create3Users();
 
 		System.out.println("Printing rows from User Table");
 		System.out.println(userDAO.readById(1));
 		System.out.println(userDAO.readById(2));
 		System.out.println(userDAO.readById(3));
 
-		return new Response("Hello, " + request.getFirstName() + " " + request.getLastName());
+		return new Response("Hello, Kasim Doctor");
+	}
+	
+	private void create3Users() {
+		System.out.println("Creating 3 Test Users");
+		User user = new User();
+		user.setFirstName("Kasim");
+		user.setLastName("Doctor");
+		user.setActive(true);
+		user.setId(1);
+
+		User user2 = new User();
+		user2.setFirstName("Queen");
+		user2.setLastName("Elizabeth");
+		user2.setActive(true);
+		user2.setId(2);
+
+		User user3 = new User();
+		user3.setFirstName("Adolf");
+		user3.setLastName("Hitler");
+		user3.setActive(false);
+		user3.setId(3);
+
+		userDAO.addUser(user);
+		userDAO.addUser(user2);
+		userDAO.addUser(user3);
+	}
+	
+	private void processS3Event(S3Event s3Event, LambdaLogger logger) {
+		List<S3EventNotification.S3EventNotificationRecord> records = s3Event.getRecords();
+		logger.log("Number of records in the S3Event =  " + records.size() + "\n");
+		logger.log("The S3Event looks like this: \n");
+		S3EventNotification.S3EventNotificationRecord s3EventNotificationRecord = records.get(0);
+		logger.log(s3Event.toJson());
 	}
 
 	public void inspectContextObject(Context context) {
